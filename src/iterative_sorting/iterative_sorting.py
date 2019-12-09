dc99e19d56aae77d0fb2618d18d751ecc84a8efc
Suparada Saitalae <ann.suparada@gmail.com>
2019-12-09 17:00:17 -0600

    add comment, MVP part 1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,15 +17,15 @@ print(selection_sort([5, 1, 3, 7, 9, 2, 4, 6]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    index_length = len(arr) -1
+    index_length = len(arr) -1  # find the length to comparison and -1 because last idex can't compare
     sorted_index = False
 
     while not sorted_index:
         sorted_index = True
         for i in range(0, index_length):
-            if arr[i] > arr[i+1]:
+            if arr[i] > arr[i+1]: # [i] is the value to the left, [i+1] is the value to the rigth
                 sorted_index = False
-                arr[i], arr[i+1] = arr[i+1], arr[i]
+                arr[i], arr[i+1] = arr[i+1], arr[i] # swap the value position
 
     return arr
 
