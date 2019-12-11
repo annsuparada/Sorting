17c77a1136da847f7943975b65f872ba2c176ba6
Suparada Saitalae <ann.suparada@gmail.com>
2019-12-10 18:52:02 -0600

    merge sort done, MVP

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,18 +1,32 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
-    return merged_arr
+# TO-DO: complete the helpe function below to merge 2 sorted leftys
+def merge( left, right ):
+        merged_arr = []
+        while len(left) > 0 and len(right) > 0 :
+            if left[0] < right[0]:
+                merged_arr.append(left[0])
+                left.remove(left[0])
+            else:
+                merged_arr.append(right[0])
+                right.remove(right[0])
+        while len(left) > 0:
+            merged_arr.append(left[0])
+            left.remove(left[0])
+        while len(right) > 0:
+            merged_arr.append(right[0])
+            right.remove(right[0])
 
+        return merged_arr
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
-
-    return arr
-
+    if len(arr) <= 1:
+        return arr
+    else:
+        mid_list = len(arr) // 2
+        left = merge_sort(arr[:mid_list])
+        right = merge_sort(arr[mid_list:])
+        arr = merge(left, right)
+        return arr
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
